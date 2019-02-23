4064db9f6fecf2f784f5c3ff82851f4e286b36c2
Iqbal Ajie Wahyudin <iqbalbal25@gmail.com>
2019-02-23 16:23:33 +0700

    Init

--- login/src/main/java/iqbal/app/login/LoginActivity.kt
+++ login/src/main/java/iqbal/app/login/LoginActivity.kt
@@ -2,11 +2,16 @@ package iqbal.app.login
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import iqbal.app.myappbundle.BUNDLE_KEY
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val data = intent.getStringExtra(BUNDLE_KEY)
+        Toast.makeText(this, data, Toast.LENGTH_LONG).show()
     }
 }
--- profile/src/main/java/iqbal/app/profile/ProfileActivity.kt
+++ profile/src/main/java/iqbal/app/profile/ProfileActivity.kt
@@ -2,11 +2,15 @@ package iqbal.app.profile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val name = intent.data.lastPathSegment
+        Toast.makeText(this, name, Toast.LENGTH_LONG).show()
     }
 }
